6ee418200ba08c6ff8d63cc4a9d111491728d968
jorik <jorik>
2023-01-23 11:07:27 +0300

    Initial Commit

---
+++ lesson_7_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson_7_HW
+//
+//  Created by admin on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
